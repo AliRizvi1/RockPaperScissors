277a112e933bc958f637faac9568323dc636618a
Ali Rizvi <ali@Alis-MacBook-Pro.local>
2018-10-29 14:19:40 -0400

    Initial Commit

---
+++ Assignment2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment2
+//
+//  Created by Ali Rizvi on 10/29/18.
+//  Copyright © 2018 Ali Rizvi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
